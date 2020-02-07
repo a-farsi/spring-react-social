3baeda4ad44cf1fc61829175a9fbe4d3fe0e1d3a
a-farsi <abdelhak.farsi@gmail.com>
2020-02-07 22:48:29 +0100

    react-social and spring-social examples

---
+++ spring-social/src/main/java/com/example/springsocial/payload/SignUpRequest.java
@@ -0,0 +1,23 @@
+package com.example.springsocial.payload;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignUpRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
